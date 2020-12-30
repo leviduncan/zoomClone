949f073d21e9094a77718679d01d2f7647572524
Darrin Duncan <darrin@darrinduncan.com>
2020-12-30 15:00:29 -0500

    This is my first attempt at creating a zoom clone

---
+++ public/script.js
@@ -0,0 +1,61 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+}); 
+const myVideo = document.createElement('video')
+myVideo.muted = true // mute our own video
+const peers = {}
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+    
+    // listen for a call, answer the call and send them our stream
+    // the ability to recieve calls by listening to the on call event
+    myPeer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+    
+    // when a new user connects...
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
+})
+
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+})
+
+// ability to make calls when new users connect to our room
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+
+    peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
